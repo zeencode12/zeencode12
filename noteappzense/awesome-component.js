f5a9c2bb455264ea0f2f1fb67fba28955cbfecb6
zeencode12 <163313692+zeencode12@users.noreply.github.com>
2025-08-13 13:22:44 +0700

    Delete noteappzense directory

--- noteappzense/awesome-component.js
+++
@@ -1,8 +0,0 @@
-class AwesomeComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.innerHTML = 'Aku adalah web component terbaik.';
-  }
-}
-
-customElements.define('awesome-component', AwesomeComponent);
